9035b4457d40b1655dafa733532e279552cccba8
윤창현 <changh950@gmail.com>
2021-09-01 19:04:59 +0900

    changhyun

---
+++ src/pages/changhyunyoon/Detail/Detail.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Nav from './Nav'
+import './Detail.scss'
+
+
+class Detail extends React.Component {
+  render() {
+    return (
+      <>
+        <Nav />
+      </>   
+    )
+  }
+}
+export default Detail;
--- src/pages/changhyunyoon/Login/Login.js
+++ src/pages/changhyunyoon/Login/Login.js
@@ -1,9 +1,29 @@
-import { Component } from 'react';
+import React from 'react'
+import './Login.scss'
+import { Link } from 'react-router-dom'
 
-class ChanghyunLogin extends Component {
-  render() {
-    return <div>창현의 로그인 페이지</div>;
+
+class Login extends React.Component {
+  render() { 
+    return (
+      <section>
+      <div className="logInBox">
+      <p className="logInlogo">WeBucks</p>
+      <form className="form" action="./list.html">
+          <div>
+            <input className="text" type="text" placeholder="전화번호, 사용자 이름 또는 이메일" />
+          </div>
+          <div>
+            <input className="password" type="password" placeholder="비밀번호" />
+          </div>
+          <div>
+            <Link><button className="button" disabled="disabled">로그인</button></Link>
+          </div>
+      </form>
+          <a className="link" href="#" >비밀번호를 잊으셨나요?</a>
+      </div>
+      </section>
+   )
   }
 }
-
-export default ChanghyunLogin;
+export default Login;
