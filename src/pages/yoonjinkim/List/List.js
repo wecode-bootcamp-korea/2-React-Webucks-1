c6fbbc60a66d38c695771a3c601d1a2ddeb9132f
jin0oon <86861297+jin0oon@users.noreply.github.com>
2021-09-07 20:34:48 +0900

    Modify: [Mission1]의 map돌리기를, .json파일 이용해서, componentDidMount()에서 fetch함수 써서 받아온 데이터로 해보기

--- src/pages/yoonjinkim/List/List.js
+++ src/pages/yoonjinkim/List/List.js
@@ -1,57 +1,60 @@
 import React from 'react';
-import './List.scss';
 import Nav from '../../../components/Nav/Nav';
 import FirstCoffeeCard from './CoffeeCard/FirstCoffeeCard';
 import SecondCoffeeCard from './CoffeeCard/SecondCoffeeCard';
+import './List.scss';
 
 class Listyoonjin extends React.Component {
   constructor() {
     super();
     this.state = {
-      color: 'red',
-      products: [],
+      firstProducts: [],
+      secondProducts: [],
     };
   }
 
   componentDidMount() {
     fetch('http://localhost:3000/Data/ListCoffeeCardData.json')
-      .then(res => res.json)
-      .then(result => {
-        this.setState({ products: result.Coldbrew });
-        console.log(result.Coldbrew);
+      .then(res => res.json())
+      .then(jsonData => {
+        this.setState({
+          firstProducts: jsonData.Coldbrew,
+          secondProducts: jsonData.Brewed,
+        });
       });
   }
 
   render() {
+    console.log(this.state.products, 'vdfadsa');
     return (
       <div className="listpage-wrap">
         <Nav />
         <h2 className="list--head__coldbrew">☕️콜드 브루 커피☕️</h2>
         <div className="coffeecard-list-wrap">
-          {/* {this.state.products.map((productData, index) => {
+          {this.state.firstProducts.map(jsonFirstData => {
             return (
               <FirstCoffeeCard
-                key={index}
-                src={productData.src}
-                alt={productData.alt}
-                name={productData.name}
+                key={jsonFirstData.key}
+                src={jsonFirstData.src}
+                alt={jsonFirstData.alt}
+                name={jsonFirstData.name}
               />
             );
-          })} */}
+          })}
+          }
         </div>
-
         <h2 className="list--head__brewed">☕️브루드 커피☕️</h2>
         <div className="coffeecard-list-wrap">
-          {/* {this.state.products.map((productData, index) => {
-          return (
-            <SecondCoffeeCard
-              key={index}
-              src={productData.src}
-              alt={productData.alt}
-              name={productData.name}
-            />
-          );
-        })} */}
+          {this.state.secondProducts.map(jsonSecondData => {
+            return (
+              <SecondCoffeeCard
+                key={jsonSecondData.key}
+                src={jsonSecondData.src}
+                alt={jsonSecondData.alt}
+                name={jsonSecondData.name}
+              />
+            );
+          })}
         </div>
       </div>
     );
